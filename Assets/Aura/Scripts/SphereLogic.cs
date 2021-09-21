ea3ccb5b6b29fb5214dd913329c207a593493ac6
Jason Maina <auravision.africa@gmail.com>
2021-09-21 11:17:02 +0300

    Added Env Assets And simple sphere logic script

---
+++ Assets/Aura/Scripts/SphereLogic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SphereLogic : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    [SerializeField]private float jumpForce = 1000f;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+
+    }
+
+    private void Update()
+    {
+        //make sphere jump on space bar press
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //add force to rigidbody upwards
+            rigidBody.AddForce(Vector3.up * jumpForce);
+        }
+    }
+}
