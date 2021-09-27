b987d73733b7452895ebfa600d3c7e53b706ab15
Jason Maina <auravision.africa@gmail.com>
2021-09-27 04:25:20 +0300

    Added some cute little coins and retro coin sound just for fun.

---
+++ Assets/Aura/Scripts/CoinBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBehaviour : MonoBehaviour
+{
+    [SerializeField] float m_RotateAngle = 1.5f;
+
+    [Header("FX Variables")]
+    [SerializeField] AudioClip m_CoinFX;
+    AudioSource m_AudioSource;
+
+    Collider m_CoinCollider;
+    MeshRenderer m_MeshRenderer;
+
+    private void Start()
+    {
+        m_CoinCollider = GetComponent<Collider>();
+        m_MeshRenderer = GetComponent<MeshRenderer>();
+        //m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotate coin by certain angle amount each frame
+        transform.Rotate(Vector3.forward, m_RotateAngle);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Destroy(gameObject);
+        AudioSource.PlayClipAtPoint(m_CoinFX, transform.position);
+        m_CoinCollider.enabled = false;
+        m_MeshRenderer.enabled = false;
+        
+    }
+}
